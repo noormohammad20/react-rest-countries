99986346b858e949ffa0979863af9092a79d1a33
noormohammad20 <mdsaim204@gmail.com>
2022-03-17 00:02:02 +0600

    Components inside the folder structure and connect them

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Countries = () => {
+    const [Countries, setCountries] = useState([])
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
     return (
         <div>
-            <h2>Countries In Other File</h2>
+            <h1>Hello Form Countries: {Countries.length}</h1>
         </div>
     )
 }
